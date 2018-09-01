66917a3fac0bf070668a1b27343ecf082838083f
nantaihu <1311192566@qq.com>
2018-09-01 20:03:57 +0800

    Site updated: 2018-09-01 20:03:56

---
+++ dist/music.js
@@ -0,0 +1,21 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "最重要的小事",
+        artist: '徐梦圆',
+        url: 'http://www.ytmp3.cn/down/52243.mp3',
+        cover: 'https://wx4.sinaimg.cn/mw690/957bcbf4gy1fuphwh04mhj20ku0jy768.jpg',
+      },
+      {
+        name: '私奔到月球',
+        artist: '麦小兜',
+        url: 'http://www.ytmp3.cn/down/52244.mp3',
+        cover: 'https://wx4.sinaimg.cn/mw690/957bcbf4gy1fuphwh04mhj20ku0jy768.jpg',
+      },
+      
+    ]
+});
+
